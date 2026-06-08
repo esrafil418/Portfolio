dd98f388763604ec507eabc992c8a02cb5896c70
Esrafil <esrafil.s.r.man418@gmail.com>
2026-06-08 21:48:31 +0330

    feat(style): Add Stars Background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
+import StarBackground from "../components/StarBackground";
 import ThemeToggle from "../components/ThemeToggle";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       <ThemeToggle />
+
+      <StarBackground />
     </div>
   );
 }
