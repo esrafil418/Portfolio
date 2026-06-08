12f7849055638d0a957c6df546ee00f420e1196f
Esrafil <esrafil.s.r.man418@gmail.com>
2026-06-08 22:02:18 +0330

    feat(style): Add meteor Animation to the Background

--- src/components/StarBackground.jsx
+++ src/components/StarBackground.jsx
@@ -6,6 +6,7 @@ export default function StarBackground() {
 
   useEffect(() => {
     generateStars();
+    generateMeteors();
   }, []);
 
   const generateStars = () => {
@@ -28,6 +29,24 @@ export default function StarBackground() {
     setStars(newStars);
   };
 
+  const generateMeteors = () => {
+    const numberOfMeteors = 4;
+
+    const newMeteors = [];
+
+    for (let i = 0; i < numberOfMeteors; i++) {
+      newMeteors.push({
+        id: i,
+        size: Math.random() * 2 + 1,
+        x: Math.random() * 100,
+        y: Math.random() * 20,
+        delay: Math.random() * 15,
+        animationDuration: Math.random() * 3 + 3,
+      });
+    }
+    setMeteors(newMeteors);
+  };
+
   return (
     <div className="fixed inset-0 overflow-hidden pointer-events-none z-0">
       {stars.map((star) => (
@@ -41,6 +60,21 @@ export default function StarBackground() {
             top: star.y + "%",
             opacity: star.opacity,
             animationDuration: star.animationDuration + "s",
+          }}
+        />
+      ))}
+
+      {meteors.map((meteor) => (
+        <div
+          key={meteor.id}
+          className="meteor animate-meteor"
+          style={{
+            width: meteor.size * 20 + "px",
+            height: meteor.size * -1 + "px",
+            left: meteor.x + "%",
+            top: meteor.y + "%",
+            animationDelay: meteor.delay,
+            animationDuration: meteor.animationDuration + "s",
           }}
         />
       ))}
