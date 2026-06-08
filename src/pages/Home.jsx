fb249a3b78c26a1fa580779385d2b103f6e8fdb3
Esrafil <esrafil.s.r.man418@gmail.com>
2026-06-08 20:35:21 +0330

    feat: Add Theme Toggle Component and Fix utility Error in index.css File

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
+import ThemeToggle from "../components/ThemeToggle";
+
 export default function Home() {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
-      Home
+      <ThemeToggle />
     </div>
   );
 }
