4c6624183c3218e0c3f59b731a5f018e342b396d
Esrafil <esrafil.s.r.man418@gmail.com>
2026-06-24 21:06:52 +0330

    feat: implement Markdown

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Intro from "@/components/intro";
+import { MDXRemote } from "next-mdx-remote/rsc";
 
 export default function page() {
+	const content = ` # Test Markdown `;
 	return (
 		<section className="py-24">
 			<div className="container max-w-3xl">
 				<Intro />
+				<MDXRemote source={content} />
 			</div>
 		</section>
 	);
